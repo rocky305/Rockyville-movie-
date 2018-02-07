9aab2fcf5c4d7c2d9cdbafcf081aa2c446db5df1
rkaton1749 <rkaton1749@my.sctd.edu>
2018-02-07 17:32:39 -0500

    Add project files.

---
+++ Rockyville movie tickets/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rockyville_movie_tickets.MvcApplication" Language="C#" %>
---
+++ Rockyville movie tickets/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rockyville_movie_tickets.Startup))]
+namespace Rockyville_movie_tickets
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
